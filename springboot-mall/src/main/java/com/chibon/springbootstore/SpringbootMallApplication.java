219977e3fafa1ef82681106ee3d0063c4a607bbf
ChiBon <73291132+ChiChiBonBon@users.noreply.github.com>
2023-11-06 22:48:44 +0800

    init Spring Boot project

---
+++ springboot-mall/src/main/java/com/chibon/springbootstore/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.chibon.springbootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
